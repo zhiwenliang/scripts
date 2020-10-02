d8f98f43052a8ace9577b85c39b77af04780d14b
zhiwen <zhiwen_liang@outlook.com>
2020-10-03 01:42:20 +0800

    use args to transfer VPS's IP

--- remote_install_v2ray.sh
+++ remote_install_v2ray.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-ssh-keygen -f "/home/alpha/.ssh/known_hosts" -R "149.28.60.133"
-ssh -o StrictHostKeyChecking=no root@149.28.60.133 "sh " < ./v2ray.sh
+ip=$(echo $1|awk '{split($0,a,"@");print a[2]}')
+ssh-keygen -f "/home/alpha/.ssh/known_hosts" -R "$ip"
+ssh -o StrictHostKeyChecking=no $1 "sh " < ./v2ray.sh
