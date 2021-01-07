4497c610a84eb39f476ce9ef176157815afc5b7e
zhiwen liang <zhiwen_liang@outlook.com>
2021-01-07 08:44:00 +0800

    update v2ray version on v2ray.sh

--- vps/v2ray.sh
+++ vps/v2ray.sh
@@ -1,7 +1,7 @@
 apt install unzip -y
 curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh &&
 bash install-release.sh &&
-wget https://github.com/v2fly/v2ray-core/releases/download/v4.27.0/v2ray-linux-64.zip &&
+wget https://github.com/v2fly/v2ray-core/releases/download/v4.34.0/v2ray-linux-64.zip &&
 unzip ~/v2ray-linux-64.zip "vpoint_vmess_freedom.json" &&
 sed -i "3s/.*/\"port\": 12345,/g" ~/vpoint_vmess_freedom.json &&
 sed -i "8s/.*/\"id\": \"e15a7a6f-8b5c-43d0-958a-11fe1dcb79ea\",/g" ~/vpoint_vmess_freedom.json &&
