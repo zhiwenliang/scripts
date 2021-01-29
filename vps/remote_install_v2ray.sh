5c88525d44cfc699650794f9849627e77d3df61b
zhiwen <zhiwen_liang@outlook.com>
2021-01-29 15:13:17 +0800

    add script

--- vps/remote_install_v2ray.sh
+++ vps/remote_install_v2ray.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dir=$(cd "$(dirname "$0")";pwd)
-ssh-keygen -f "/home/alpha/.ssh/known_hosts" -R "149.28.60.133"
-ssh -o StrictHostKeyChecking=no root@149.28.60.133 "sh " < $dir/v2ray.sh
+ssh-keygen -f "/home/alpha/.ssh/known_hosts" -R "149.28.235.28"
+ssh -o StrictHostKeyChecking=no root@149.28.235.28 "sh " < $dir/v2ray.sh
