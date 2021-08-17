1c6625c1c57f7063c84e7f557e50d966b90cc420
Zhiwen Liang <zhiwen_liang@outlook.com>
2021-08-17 16:44:32 +0800

    Update remote install v2ray script on Windows platform.

--- vps/remote_install_v2ray.ps1
+++ vps/remote_install_v2ray.ps1
@@ -2,4 +2,4 @@ param($target)
 $ip=$target.split("@")[1]
 ssh-keygen -f "C:\Users\alpha\.ssh\known_hosts" -R "$ip"
 scp v2ray.sh ${target}:~/
-ssh -o StrictHostKeyChecking=no $target "sh ~/v2ray.sh"
+ssh -o StrictHostKeyChecking=no $target "curl -LO https://raw.githubusercontent.com/zhiwenliang/scripts/main/vps/v2ray.sh && bash ~/v2ray.sh"
