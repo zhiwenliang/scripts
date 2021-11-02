e10900102d457f35253efa25f062bfd28f9ed76a
Zhiwen Liang <zhiwen_liang@outlook.com>
2021-11-02 12:29:40 +0800

    fix v2ray.sh; add frps.sh

---
+++ vps/frps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt update
+apt install -y iptables iptables-persistent
+curl -LO https://github.com/fatedier/frp/releases/download/v0.38.0/frp_0.38.0_linux_amd64.tar.gz
+tar -xvf frp_0.38.0_linux_amd64.tar.gz
+iptables -I INPUT -p tcp --dport 6000 -j ACCEPT &&
+iptables -I INPUT -p tcp --dport 7000 -j ACCEPT &&
+iptables-save &&
+netfilter-persistent save &&
+netfilter-persistent reload && 
+nohup ./frps -c ./frps.ini &
