479ecb56c8933c920037b7cdf5ecc8fdf7355f4a
Zhiwen Liang <zhiwen_liang@outlook.com>
2021-11-02 12:20:44 +0800

    fix v2ray.sh

--- vps/v2ray.sh
+++ vps/v2ray.sh
@@ -4,11 +4,11 @@ apt install unzip iptables iptables-persistent -y
 curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh &&
 bash install-release.sh &&
 curl -LO https://raw.githubusercontent.com/zhiwenliang/scripts/main/vps/config.json &&
-mv ~/config.json /usr/local/etc/v2ray/config.json && 
+mv ./config.json /usr/local/etc/v2ray/config.json && 
 apt-get update &&
 iptables -I INPUT -p tcp --dport 6666 -j ACCEPT &&
 iptables-save &&
 netfilter-persistent save &&
-netfilter-persistent reload &&
+netfilter-persistent reload &&  
 systemctl enable v2ray && 
 systemctl restart v2ray
