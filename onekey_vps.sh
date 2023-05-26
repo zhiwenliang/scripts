4ccbb84d8b01a7b3835177b3aa873b018afa35b7
liangzhiwen <zhiwen_liang@outlook.com>
2023-05-27 00:39:03 +0800

    update vps install script

--- onekey_vps.sh
+++ onekey_vps.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+apt update
 apt install unzip iptables iptables-persistent -y
 iptables -I INPUT -p tcp --dport 6666 -j ACCEPT &&
-iptables-save &&
-netfilter-persistent save &&
-netfilter-persistent reload 
+    iptables-save &&
+    netfilter-persistent save &&
+    netfilter-persistent reload
 bash <(curl -s -L https://git.io/v2ray.sh)
