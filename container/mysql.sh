84c61ad8014dfd503a4f9b927a6548aed836b384
zhiwenliang <zhiwen_liang@outlook.com>
2024-03-04 20:25:01 +0800

    update mysql container script

--- container/mysql.sh
+++ container/mysql.sh
@@ -1,1 +1,1 @@
-podman run -itd --name mysql-demo -p 3306:3306 -v /home/alpha/source/db/mysql/data:/var/lib/mysql mysql
+podman run -itd --name mysql-demo -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mysql -v /home/alpha/source/db/mysql/data:/var/lib/mysql mysql
