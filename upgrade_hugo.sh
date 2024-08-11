b4409257ed595f9f92d4c9f0979642302f1b7ce7
zhiwenliang <zhiwen_liang@outlook.com>
2024-08-11 18:04:32 +0800

    upgrade hugo script

--- upgrade_hugo.sh
+++ upgrade_hugo.sh
@@ -1,8 +1,7 @@
 #! /usr/bin/bash
+latest_version=$(curl -sL https://github.com/gohugoio/hugo/releases/latest | grep -oP '(?<=href="/gohugoio/hugo/releases/tag/v)[^"]*')
 upgrade() {
     # get download url and download
-    latest_version=$(curl -sL https://github.com/gohugoio/hugo/releases/latest | grep -oP '(?<=href="/gohugoio/hugo/releases/tag/v)[^"]*')
-    echo "Latest version of hugo is $latest_version"
     file_name="hugo_extended_${latest_version}_linux-amd64.tar.gz"
     download_url="https://github.com/gohugoio/hugo/releases/download/v${latest_version}/${file_name}"
     echo "Download url is：$download_url"
@@ -30,9 +29,9 @@ fi
 # Get the current version of Go installed on the system
 current_version=$(hugo version | awk '{print $2}' | awk -F'-' '{print $1}' | awk -F'v' '{print $2}')
 
-echo "current version is $current_version"
 
 # Check if the current version is same as the latest version
+echo "current_version: $current_version; latest_version: $latest_version"
 if [ "$current_version" == "$latest_version" ]; then
     echo "Hugo is already up to date."
     exit 0
